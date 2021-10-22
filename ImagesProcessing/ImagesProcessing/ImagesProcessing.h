e3eddcf11fffe414298b3dfae3d2d171e757f9e8
nmikolaychuk <knopka420@gmail.com>
2021-10-22 21:41:55 +0300

    Добавление на контроль.

---
+++ ImagesProcessing/ImagesProcessing/ImagesProcessing.h
@@ -0,0 +1,32 @@
+﻿
+// ImagesProcessing.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CImagesProcessingApp:
+// Сведения о реализации этого класса: ImagesProcessing.cpp
+//
+
+class CImagesProcessingApp : public CWinApp
+{
+public:
+	CImagesProcessingApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CImagesProcessingApp theApp;
---
+++ ImagesProcessing/ImagesProcessing/ImagesProcessingDlg.h
@@ -0,0 +1,34 @@
+﻿
+// ImagesProcessingDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CImagesProcessingDlg
+class CImagesProcessingDlg : public CDialogEx
+{
+// Создание
+public:
+	CImagesProcessingDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_IMAGESPROCESSING_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
